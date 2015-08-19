c190575e82e6b4fd89797aaca87ee6280fc6efcb
Herve Pages <hpages@fhcrc.org>
2015-08-19 23:51:55 +0000

    Make src/Makevars portable by using = instead of GNU extension += . Two reasons for this change: (1) = seems to work just fine (so why would I use += in the first place?) and (2) 'R CMD check' was complaining about this.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/XVector@107603 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/io-utils.R
+++ R/io-utils.R
@@ -11,12 +11,11 @@ new_input_filexp <- function(filepath)
     .Call2("new_input_filexp", filepath, PACKAGE="XVector")
 }
 
-new_output_filexp <- function(filepath, append,
-                                       compress, compression_level)
+new_output_filexp <- function(filepath, append, compress, compression_level)
 {
-    .Call2("new_output_filexp", filepath, append,
-                                compress, compression_level,
-                                PACKAGE="XVector")
+    .Call2("new_output_filexp", filepath,
+           append, compress, compression_level,
+           PACKAGE="XVector")
 }
 
 finalize_filexp <- function(filexp)
